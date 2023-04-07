256d3380784c09694333a633804a6ce79b0423a7
Akhmed <a-kadymov@yandex.ru>
2023-04-07 17:35:10 +0400

    socket_host

--- api/nagels-socket-server.py
+++ api/nagels-socket-server.py
@@ -1,7 +1,6 @@
 from app import app, db, socketio
 from app.models import User, Room, Game, Hand, Turn, Player, TurnCard, DealtCards, HandScore
 from config import get_settings, get_environment
-from app import socket
 
 
 env = get_environment()
@@ -14,4 +13,5 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=flask_configs['DEBUG'][env], port=flask_configs['SOCKET_PORT'][env])
+    socketio.run(app, debug=flask_configs['DEBUG'][env], host=flask_configs['SOCKET_HOST'][env], port=flask_configs['SOCKET_PORT'][env])
+
