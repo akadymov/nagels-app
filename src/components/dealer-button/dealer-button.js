7fa65a3aec69cd9833d5abe6b8707908596011bd
Akhmed <a-kadymov@yandex.ru>
2023-04-11 00:16:24 +0400

    shark bckgr in dealer button

--- src/components/dealer-button/dealer-button.js
+++ src/components/dealer-button/dealer-button.js
@@ -7,7 +7,6 @@ export default class DealerButton extends React.Component{
     render () {
         return(
             <div className={`dealer-button ${this.props.position} ${ this.props.isMobile ? "mobile" : (this.props.isDesktop ? "desktop" : "tablet")} ${ this.props.isPortrait ? "portrait" : "landscape"}`}>
-                D
             </div>
         )
     }
